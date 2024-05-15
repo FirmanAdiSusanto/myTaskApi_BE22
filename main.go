eccd1d4596f772f4dd405dc1fe53fe454337de0a
Firman <firmanadisusantoo@gmail.com>
2024-05-15 14:12:06 +0700

    Feat:deploy

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"taskApi/app/configs"
+	"taskApi/app/databases"
+	"taskApi/app/routers"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	cfg := configs.InitConfig()
+	dbMysql := databases.InitDBMysql(cfg)
+	// dbPosgres := databases.InitDBPosgres(cfg)
+
+	// create new instance echo
+	e := echo.New()
+
+	routers.InitRouter(e, dbMysql)
+	e.Use(middleware.Logger())
+	e.Use(middleware.CORS())
+	// e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+	// 	Format: "method=${method}, uri=${uri}, status=${status}\n",
+	// }))
+	// start server and port
+	e.Logger.Fatal(e.Start(":8080"))
+}
